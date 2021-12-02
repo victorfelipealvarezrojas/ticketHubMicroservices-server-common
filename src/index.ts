55544cff55c5833072394ab746be6d4f18ee2660
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2021-12-02 14:44:48 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,6 @@ export * from './events/order-created-event';
 export * from './events/order-cancelled-event';
 export * from './events/subjects';
 export * from './events/types/order-status';
-
+export * from './events/expiration-complete-event';
 
 //npm run pub
